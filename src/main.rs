b3bb8b5743641c75d1faaa2662ed5236acaa0910
Richard <esanrichard1@gmail.com>
2024-11-28 13:28:13 +0100

    last update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// fn main() {
+//     let mut x  = 5;
+//     println!("The value of x is : {x}");
+//     x = 6;
+//     const y: u32 = 60 * 60 * 3;
+
+//     println!("The value of x is : {x} {y}");
+
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
 }
